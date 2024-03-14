98227ca8302e5f3bfe37cda60b7afc9c893b8ca1
Bear Killer <kenneth.byrkett@gmail.com>
2024-03-14 13:07:03 -0500

    [1.02] Initial Commit  - Set up console environment  - Installed NLog 5.1.1

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using NLog;
+
+namespace MediaLibrary;
+class Program
+{
+    public abstract class Media {
+
+            //public virtual string Display() {
+
+            //}
+
+    }
+
+    static void Main(string[] args) {
+
+    string path = Directory.GetCurrentDirectory() + "\\nlog.config";
+
+    // create instance of Logger
+    var logger = LogManager.LoadConfiguration(path).GetCurrentClassLogger();
+    logger.Info("Program started");
+
+    Console.WriteLine("Hello World!");
+
+    logger.Info("Program ended");
+
+    }
+
+}
