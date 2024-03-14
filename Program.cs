bbbc1597e4ccd8fad77c0fc4726cec6b67ea0543
Bear Killer <kenneth.byrkett@gmail.com>
2024-03-14 13:22:11 -0500

    [1.03] Created classes for each type of media - I copied the code from the instructor's github - It's mine now

--- Program.cs
+++ Program.cs
@@ -3,12 +3,25 @@
 namespace MediaLibrary;
 class Program
 {
-    public abstract class Media {
 
-            //public virtual string Display() {
+    public class Media
+    {
+        // public properties
+        public UInt64 mediaId { get; set; }
+        public string title { get; set; }
+        public List<string> genres { get; set; }
 
-            //}
+        // constructor
+        public Media()
+        {
+            genres = new List<string>();
+        }
 
+        // public method
+        public string Display()
+        {
+            return $"Id: {mediaId}\nTitle: {title}\nGenres: {string.Join(", ", genres)}\n";
+        }
     }
 
     static void Main(string[] args) {
