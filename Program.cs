8d44b8b9075381584c957e7d60823d847d0972e6
BearKiller <kenneth.byrkett@gmail.com>
2024-03-21 15:08:07 -0500

    [1.05] Added console menu to ask user if they'd like to add or display movies

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
 using NLog;
 
 // See https://aka.ms/new-console-template for more information
@@ -11,5 +12,24 @@ logger.Info("Program started");
 string scrubbedFile = FileScrubber.ScrubMovies("movies.csv");
 logger.Info(scrubbedFile);
 MovieFile movieFile = new MovieFile(scrubbedFile);
+
+string option;
+do {
+    Console.Clear();
+    Console.WriteLine("1) Add a Movie");
+    Console.WriteLine("2) Display All Movies");
+    Console.WriteLine("Press enter to quit");
+    Console.Write("> ");
+    option = Console.ReadLine();
+    switch(option) {
+    // Menu to add movies
+        case "1":
+        break;
+
+    // Menu to display all movies
+        case "2":
+        break;
+    }
+} while (option == "1" || option == "2");
 
 logger.Info("Program ended");
