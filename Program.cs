23d22d5888e64385a10fd7ad795ca002832c3f0a
BearKiller <kenneth.byrkett@gmail.com>
2024-03-25 12:14:32 -0500

    [1.06] Added menu to display all movies from the Movies class.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Runtime.InteropServices;
 using System.Security.Cryptography.X509Certificates;
 using NLog;
+using Helper;
 
 // See https://aka.ms/new-console-template for more information
 string path = Directory.GetCurrentDirectory() + $"{Path.DirectorySeparatorChar}nlog.config";
@@ -13,9 +14,10 @@ string scrubbedFile = FileScrubber.ScrubMovies("movies.csv");
 logger.Info(scrubbedFile);
 MovieFile movieFile = new MovieFile(scrubbedFile);
 
+
 string option;
 do {
-    Console.Clear();
+    //Console.Clear();
     Console.WriteLine("1) Add a Movie");
     Console.WriteLine("2) Display All Movies");
     Console.WriteLine("Press enter to quit");
@@ -24,10 +26,27 @@ do {
     switch(option) {
     // Menu to add movies
         case "1":
+        logger.Info("User choice: \"1\"");
         break;
 
     // Menu to display all movies
         case "2":
+        logger.Info("User choice: \"2\"");
+        if (File.Exists(scrubbedFile)) {
+            logger.Info("movies.scrubbed.csv located.");
+            Console.WriteLine(movieFile.Movies.Count);
+            foreach (Movie movie in movieFile.Movies) {
+                // Output movie details
+                Console.WriteLine($"Title: {movie.title}");
+                Console.WriteLine($"Genres: {string.Join(", ", movie.genres)}");
+                Console.WriteLine($"Director: {movie.director}");
+                Console.WriteLine($"Running Time: {movie.runningTime}");
+                Console.WriteLine();
+            }
+            
+        } else {
+            logger.Warn("movies.scrubbed.csv is missing.");
+        }
         break;
     }
 } while (option == "1" || option == "2");
